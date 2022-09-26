3b879b17c3db3ab0b5234a3631488523b8111cbc
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-09-26 21:48:07 +0930

    Exercise 10.14: storing the access token step1

--- rate-repository-app/src/components/common/Text.jsx
+++ rate-repository-app/src/components/common/Text.jsx
@@ -1,6 +1,6 @@
 import { Platform, Text as NativeText, StyleSheet } from 'react-native';
 
-import theme from '../../theme';
+import theme from '../../utils/theme';
 
 const styles = StyleSheet.create({
   text: {
--- rate-repository-app/src/hooks/useSignIn.js
+++ rate-repository-app/src/hooks/useSignIn.js
@@ -18,6 +18,7 @@ const useSignIn = () => {
         }
       }
     })
+
     return tokenResponse
   };
 
